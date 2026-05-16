fc4b2addd05a71275bee04721068df2d9990f7d2
René Jessen <rene@pasionel.dk>
2026-05-16 14:41:45 +0200

    Clean source install output

--- install-macos.sh
+++ install-macos.sh
@@ -5,8 +5,12 @@ SOURCE_DIR="$(cd "$(dirname "$0")" && pwd)"
 TARGET_DIR="$HOME/Library/Application Support/Adobe/CEP/extensions/com.rj.qrcodeillustrator"
 
 mkdir -p "$(dirname "$TARGET_DIR")"
-rm -rf "$TARGET_DIR"
-cp -R "$SOURCE_DIR" "$TARGET_DIR"
+rsync -a --delete --delete-excluded \
+  --exclude ".git" \
+  --exclude ".DS_Store" \
+  --exclude "dist" \
+  --exclude "node_modules" \
+  "$SOURCE_DIR/" "$TARGET_DIR/"
 
 for version in 11 12 13 14; do
   defaults write "com.adobe.CSXS.$version" PlayerDebugMode 1
